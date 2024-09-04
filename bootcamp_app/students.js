2d12f9635dfe1c697ff104738394d217f95ceb10
Benjamin <bendhallam@gmail.com>
2024-09-04 08:49:04 -0600

    added parametrization

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -12,21 +12,20 @@ const queryString = `
   FROM students
   JOIN cohorts ON cohorts.id = cohort_id
   WHERE cohorts.name LIKE $1
-  LIMIT $2
-  `;
+  LIMIT $2;
+`;
 
 const cohortName = process.argv[2];
 const limit = process.argv[3] || 5;
 // Store all potentially malicious values in an array
 const values = [`%${cohortName}%`, limit];
 
-
-pool
-  .query(queryString, values)
-  .then((res) => {
-    res.rows.forEach((user) => {
-      console.log(
-        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
-      );
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.name}: ${row.cohort}`);
     });
+  })
+  .catch(err => {
+    console.error('Error executing query', err.stack);
   });
